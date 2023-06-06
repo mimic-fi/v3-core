8a0d040dcfe30cd2489cebc71b19559da035b693
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-06-06 12:32:10 -0300

    registry: implement mimic implementations registry

---
+++ packages/registry/contracts/interfaces/IRegistry.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+import './IRegistry.sol';
+
+/**
+ * @dev Registry interface
+ */
+interface IRegistry {
+    /**
+     * @dev Emitted every time an implementation is registered
+     */
+    event Registered(address indexed implementation, string name);
+
+    /**
+     * @dev Emitted every time an implementation is deprecated
+     */
+    event Deprecated(address indexed implementation);
+
+    /**
+     * @dev Tells whether an implementation is registered
+     * @param implementation Address of the implementation being queried
+     */
+    function isRegistered(address implementation) external view returns (bool);
+
+    /**
+     * @dev Tells whether an implementation is deprecated
+     * @param implementation Address of the implementation being queried
+     */
+    function isDeprecated(address implementation) external view returns (bool);
+
+    /**
+     * @dev Creates and registers an implementation
+     * @param name Name of the implementation
+     * @param code Code of the implementation to create and register
+     */
+    function create(string memory name, bytes memory code) external;
+
+    /**
+     * @dev Registers an implementation
+     * @param name Name of the implementation
+     * @param implementation Address of the implementation to be registered
+     */
+    function register(string memory name, address implementation) external;
+
+    /**
+     * @dev Deprecates an implementation
+     * @param implementation Address of the implementation to be deprecated
+     */
+    function deprecate(address implementation) external;
+}
---
+++ packages/registry/test/Registry.test.ts
@@ -0,0 +1,190 @@
+import { assertEvent, deploy, getCreationCode, getSigners } from '@mimic-fi/v3-helpers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { expect } from 'chai'
+import { Contract } from 'ethers'
+
+describe('Registry', () => {
+  let registry: Contract, owner: SignerWithAddress, other: SignerWithAddress
+
+  // eslint-disable-next-line no-secrets/no-secrets
+  const implementation = '0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5'
+
+  before('set up signers', async () => {
+    // eslint-disable-next-line prettier/prettier
+    [, owner, other] = await getSigners()
+  })
+
+  beforeEach('create registry', async () => {
+    registry = await deploy('Registry', [owner.address])
+  })
+
+  describe('initialization', () => {
+    it('sets the owner correctly', async () => {
+      expect(await registry.owner()).to.be.equal(owner.address)
+    })
+
+    it('starts with all implementations unregistered and not-deprecated', async () => {
+      expect(await registry.isRegistered(implementation)).to.be.false
+      expect(await registry.isDeprecated(implementation)).to.be.false
+    })
+  })
+
+  describe('create', () => {
+    let bytecode: string
+    const name = 'implementation@0.0.1'
+
+    beforeEach('set bytecode', async () => {
+      bytecode = await getCreationCode('Registry', [owner.address])
+    })
+
+    context('when the sender is authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(owner)
+      })
+
+      context('when the requested implementation was not created', () => {
+        context('when the requested implementation is not registered', () => {
+          it('creates and registers the requested implementation', async () => {
+            const tx = await registry.create(name, bytecode)
+
+            const event = await assertEvent(tx, 'Registered', { name })
+
+            const implementation = event.args.implementation
+            expect(await registry.isRegistered(implementation)).to.be.true
+            expect(await registry.isDeprecated(implementation)).to.be.false
+          })
+        })
+
+        context('when the requested implementation is registered', () => {
+          beforeEach('register', async () => {
+            await registry.register(name, implementation)
+          })
+
+          it('reverts', async () => {
+            await expect(registry.register(name, implementation)).to.be.revertedWith('REGISTRY_IMPL_ALREADY_REGISTERED')
+          })
+        })
+      })
+
+      context('when the requested implementation was created', () => {
+        beforeEach('create', async () => {
+          await registry.create(name, bytecode)
+        })
+
+        it('reverts', async () => {
+          await expect(registry.create(name, bytecode)).to.be.revertedWith('DEPLOYMENT_FAILED')
+        })
+      })
+    })
+
+    context('when the sender is not authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(other)
+      })
+
+      it('reverts', async () => {
+        await expect(registry.create(name, implementation)).to.be.revertedWith('Ownable: caller is not the owner')
+      })
+    })
+  })
+
+  describe('register', () => {
+    const name = 'implementation@0.0.1'
+
+    context('when the sender is authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(owner)
+      })
+
+      context('when the requested implementation is not registered', () => {
+        it('registers the requested implementation', async () => {
+          await registry.register(name, implementation)
+
+          expect(await registry.isRegistered(implementation)).to.be.true
+          expect(await registry.isDeprecated(implementation)).to.be.false
+        })
+
+        it('emits an event', async () => {
+          const tx = await registry.register(name, implementation)
+
+          await assertEvent(tx, 'Registered', { name, implementation })
+        })
+      })
+
+      context('when the requested implementation is registered', () => {
+        beforeEach('register', async () => {
+          await registry.register(name, implementation)
+        })
+
+        it('reverts', async () => {
+          await expect(registry.register(name, implementation)).to.be.revertedWith('REGISTRY_IMPL_ALREADY_REGISTERED')
+        })
+      })
+    })
+
+    context('when the sender is not authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(other)
+      })
+
+      it('reverts', async () => {
+        await expect(registry.register(name, implementation)).to.be.revertedWith('Ownable: caller is not the owner')
+      })
+    })
+  })
+
+  describe('deprecate', () => {
+    context('when the sender is authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(owner)
+      })
+
+      context('when the requested implementation is not registered', () => {
+        it('reverts', async () => {
+          await expect(registry.deprecate(implementation)).to.be.revertedWith('REGISTRY_IMPL_NOT_REGISTERED')
+        })
+      })
+
+      context('when the requested implementation is registered', () => {
+        beforeEach('register', async () => {
+          await registry.register('implementation@0.0.1', implementation)
+        })
+
+        context('when the requested implementation is not deprecated', () => {
+          it('registers the requested implementation', async () => {
+            await registry.deprecate(implementation)
+
+            expect(await registry.isRegistered(implementation)).to.be.true
+            expect(await registry.isDeprecated(implementation)).to.be.true
+          })
+
+          it('emits an event', async () => {
+            const tx = await registry.deprecate(implementation)
+
+            await assertEvent(tx, 'Deprecated', { implementation })
+          })
+        })
+
+        context('when the requested implementation is deprecated', () => {
+          beforeEach('deprecate', async () => {
+            await registry.deprecate(implementation)
+          })
+
+          it('reverts', async () => {
+            await expect(registry.deprecate(implementation)).to.be.revertedWith('REGISTRY_IMPL_ALREADY_DEPRECATED')
+          })
+        })
+      })
+    })
+
+    context('when the sender is not authorized', () => {
+      beforeEach('set sender', () => {
+        registry = registry.connect(other)
+      })
+
+      it('reverts', async () => {
+        await expect(registry.deprecate(implementation)).to.be.revertedWith('Ownable: caller is not the owner')
+      })
+    })
+  })
+})
