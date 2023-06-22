a11a912fb5350170bbdea66fb4f1414cbd423025
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-06-22 07:08:26 -0300

    connectors: implement paraswap v5 swap connector

---
+++ packages/connectors/src/paraswap-v5.ts
@@ -0,0 +1,103 @@
+import { bn, currentTimestamp, MINUTE, pct } from '@mimic-fi/v3-helpers'
+import axios, { AxiosError } from 'axios'
+import { BigNumber, Contract } from 'ethers'
+
+const PARASWAP_URL = 'https://apiv5.paraswap.io'
+
+export type PricesResponse = { data: { priceRoute: { [key: string]: string } } }
+
+export type TransactionsResponse = { data: { data: string; sig: string; signer: string } }
+
+export type SwapData = {
+  data: string
+  sig: string
+  signer: string
+  minAmountOut: BigNumber
+  expectedAmountOut: BigNumber
+}
+
+export async function getParaswapSwapData(
+  chainId: number,
+  sender: Contract,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  amountIn: BigNumber,
+  slippage: number
+): Promise<SwapData> {
+  const prices = await getPrices(chainId, sender, tokenIn, tokenOut, amountIn)
+  const priceRoute = prices.data.priceRoute
+
+  try {
+    const { destAmount } = priceRoute
+    const minAmountOut = bn(destAmount).sub(pct(bn(destAmount), slippage))
+    const transactions = await postTransactions(chainId, sender, tokenIn, tokenOut, amountIn, minAmountOut, priceRoute)
+    const { data, sig, signer } = transactions.data
+    return { data, sig, signer, minAmountOut, expectedAmountOut: bn(destAmount) }
+  } catch (error) {
+    if (error instanceof AxiosError) throw Error(error.toString() + ' - ' + error.response?.data?.error)
+    else throw error
+  }
+}
+
+export async function getPrices(
+  chainId: number,
+  sender: Contract,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  amountIn: BigNumber
+): Promise<PricesResponse> {
+  return axios.get(`${PARASWAP_URL}/prices`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    params: {
+      srcToken: tokenIn.address,
+      srcDecimals: await tokenIn.decimals(),
+      destToken: tokenOut.address,
+      destDecimals: await tokenOut.decimals(),
+      amount: amountIn.toString(),
+      side: 'SELL',
+      network: chainId,
+      userAddress: sender.address,
+    },
+  })
+}
+
+export async function postTransactions(
+  chainId: number,
+  sender: Contract,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  amountIn: BigNumber,
+  minAmountOut: BigNumber,
+  priceRoute: { [key: string]: string }
+): Promise<TransactionsResponse> {
+  const { ethers } = await import('hardhat')
+  return axios.post(
+    `${PARASWAP_URL}/transactions/${chainId}`,
+    {
+      srcToken: tokenIn.address,
+      destToken: tokenOut.address,
+      srcAmount: amountIn.toString(),
+      srcDecimals: await tokenIn.decimals(),
+      destAmount: minAmountOut.toString(),
+      destDecimals: await tokenOut.decimals(),
+      userAddress: sender.address,
+      receiver: sender.address,
+      deadline: (await currentTimestamp()).add(MINUTE).toString(),
+      priceRoute,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: {
+        gasPrice: (await ethers.provider.getGasPrice()).toString(),
+        signCalldata: true,
+        ignoreChecks: true,
+      },
+    }
+  )
+}
---
+++ packages/connectors/test/helpers/paraswap-v5/index.ts
@@ -0,0 +1,85 @@
+import { bn, currentBlockNumber } from '@mimic-fi/v3-helpers'
+import { BigNumber, Contract } from 'ethers'
+import fs from 'fs'
+import hre from 'hardhat'
+import { HardhatNetworkConfig } from 'hardhat/types'
+import path from 'path'
+
+import { getParaswapSwapData, SwapData } from '../../../src/paraswap-v5'
+
+type Fixture = SwapData & {
+  tokenIn: string
+  tokenOut: string
+  amountIn: string
+}
+
+export async function loadOrGetParaswapSwapData(
+  chainId: number,
+  sender: Contract,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  amountIn: BigNumber,
+  slippage: number
+): Promise<SwapData> {
+  const config = hre.network.config as HardhatNetworkConfig
+  const blockNumber = config?.forking?.blockNumber?.toString() || (await currentBlockNumber()).toString()
+
+  const fixture = await readFixture(chainId, tokenIn, tokenOut, blockNumber)
+  if (fixture) {
+    return {
+      data: fixture.data,
+      sig: fixture.sig,
+      signer: fixture.signer,
+      minAmountOut: bn(fixture.minAmountOut),
+      expectedAmountOut: bn(fixture.expectedAmountOut),
+    }
+  }
+
+  const swapData = await getParaswapSwapData(chainId, sender, tokenIn, tokenOut, amountIn, slippage)
+  await saveFixture(chainId, tokenIn, tokenOut, amountIn, swapData, blockNumber)
+  return swapData
+}
+
+async function readFixture(
+  chainId: number,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  blockNumber: string
+): Promise<Fixture | undefined> {
+  const swapPath = `${await tokenIn.symbol()}-${await tokenOut.symbol()}.json`
+  const fixturePath = path.join(__dirname, 'fixtures', chainId.toString(), blockNumber, swapPath)
+  if (!fs.existsSync(fixturePath)) return undefined
+  return JSON.parse(fs.readFileSync(fixturePath).toString())
+}
+
+async function saveFixture(
+  chainId: number,
+  tokenIn: Contract,
+  tokenOut: Contract,
+  amountIn: BigNumber,
+  swapData: SwapData,
+  blockNumber: string
+): Promise<void> {
+  const output = {
+    data: swapData.data,
+    sig: swapData.sig,
+    signer: swapData.signer,
+    tokenIn: tokenIn.address,
+    tokenOut: tokenOut.address,
+    amountIn: amountIn.toString(),
+    minAmountOut: swapData.minAmountOut.toString(),
+    expectedAmountOut: swapData.expectedAmountOut.toString(),
+  }
+
+  const fixturesPath = path.join(__dirname, 'fixtures')
+  if (!fs.existsSync(fixturesPath)) fs.mkdirSync(fixturesPath)
+
+  const networkPath = path.join(fixturesPath, chainId.toString())
+  if (!fs.existsSync(networkPath)) fs.mkdirSync(networkPath)
+
+  const blockNumberPath = path.join(networkPath, blockNumber)
+  if (!fs.existsSync(blockNumberPath)) fs.mkdirSync(blockNumberPath)
+
+  const swapPath = path.join(blockNumberPath, `${await tokenIn.symbol()}-${await tokenOut.symbol()}.json`)
+  fs.writeFileSync(swapPath, JSON.stringify(output, null, 2))
+}
