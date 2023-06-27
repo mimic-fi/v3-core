d4ec4a1aa2a39c18233bc2ff9d1a28c5f0cd1495
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-06-27 19:56:17 -0300

    tasks: implement curve 2crv tasks

--- packages/authorizer/contracts/AuthorizedHelpers.sol
+++ packages/authorizer/contracts/AuthorizedHelpers.sol
@@ -44,6 +44,13 @@ contract AuthorizedHelpers {
         r[1] = p2;
     }
 
+    function authParams(address p1, uint256 p2, uint256 p3) internal pure returns (uint256[] memory r) {
+        r = new uint256[](3);
+        r[0] = uint256(uint160(p1));
+        r[1] = p2;
+        r[2] = p3;
+    }
+
     function authParams(address p1, address p2, uint256 p3) internal pure returns (uint256[] memory r) {
         r = new uint256[](3);
         r[0] = uint256(uint160(p1));
