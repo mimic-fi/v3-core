59a01d8c18fa09a955c9099da74d272a7ec51afa
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-07-15 23:55:12 +0200

    Helpers: Constant addresses fixes (#73)

--- packages/helpers/src/addresses/chainlink/bsc.ts
+++ packages/helpers/src/addresses/chainlink/bsc.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-secrets/no-secrets */
 
-export const BNB_USD = '0x0567f2323251f0aab15c8dfb1967e4e8a7d42aee'
+export const BNB_USD = '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE'
 export const ETH_USD = '0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e'
 export const DAI_USD = '0x132d3C0B1D2cEa0BC552588063bdBb210FDeecfA'
 export const USDC_USD = '0x51597f405303C4377E36123cBc172b13269EA163'
