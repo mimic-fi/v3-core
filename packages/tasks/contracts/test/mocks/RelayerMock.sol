2a2c1387afbc0457cc98acf4372dc260de94368f
lgalende <lgalende@itba.edu.ar>
2023-07-20 11:42:25 -0300

    tasks: remove unnecessary library

--- packages/tasks/contracts/test/mocks/RelayerMock.sol
+++ packages/tasks/contracts/test/mocks/RelayerMock.sol
@@ -2,11 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import '@mimic-fi/v3-helpers/contracts/math/FixedPoint.sol';
-
 contract RelayerMock {
-    using FixedPoint for uint256;
-
     event Deposited(address smartVault, uint256 amount);
 
     mapping (address => uint256) public getSmartVaultBalance;
