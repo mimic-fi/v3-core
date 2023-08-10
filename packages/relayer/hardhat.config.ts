98b30304d9d95fd3fc985aebc1ea40b1fa481af8
lgalende <lau@mimic.fi>
2023-08-10 16:24:20 -0300

    relayer: override simulate method abi

--- packages/relayer/hardhat.config.ts
+++ packages/relayer/hardhat.config.ts
@@ -2,8 +2,14 @@ import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-local-networks-config-plugin'
 
+import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names'
+import { task } from 'hardhat/config'
 import { homedir } from 'os'
 import path from 'path'
+
+import overrideSimulateFunction from './src/overrideSimulateFunction'
+
+task(TASK_COMPILE).setAction(overrideSimulateFunction)
 
 export default {
   localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic.json'),
