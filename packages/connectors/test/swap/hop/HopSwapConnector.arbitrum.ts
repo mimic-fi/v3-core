6620ff90f24dd6a8b00264c5eff44fb85f070324
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-10-18 14:11:54 -0300

    helpers: add new arbitrum USDC address

--- packages/connectors/test/swap/hop/HopSwapConnector.arbitrum.ts
+++ packages/connectors/test/swap/hop/HopSwapConnector.arbitrum.ts
@@ -6,7 +6,7 @@ import { itBehavesLikeHopSwapConnector } from './HopSwapConnector.behavior'
 
 const CHAIN = 42161
 
-const USDC = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'
+const USDCe = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'
 const HUSDC = '0x0ce6c85cf43553de10fc56ceca0aef6ff0dd444d'
 const WHALE = '0x5bdf85216ec1e38d6458c870992a69e38e03f7ef'
 const HOP_USDC_DEX = '0x10541b07d8ad2647dc6cd67abd4c03575dade261'
@@ -18,5 +18,5 @@ describe('HopSwapConnector', () => {
     this.connector = await deploy('HopSwapConnector')
   })
 
-  itBehavesLikeHopSwapConnector(CHAIN, USDC, HUSDC, HOP_USDC_DEX, WHALE, SLIPPAGE)
+  itBehavesLikeHopSwapConnector(CHAIN, USDCe, HUSDC, HOP_USDC_DEX, WHALE, SLIPPAGE)
 })
