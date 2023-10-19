09ce0345b8514bebefc63e2c597e291b47a1ec8c
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-10-19 11:10:52 -0300

    Helpers: Add base protocols addresses (#114)

---
+++ packages/helpers/src/addresses/tokens/base.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-secrets/no-secrets */
+
+export const WETH = '0x4200000000000000000000000000000000000006'
+export const DAI = '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb'
+export const USDC = '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'
