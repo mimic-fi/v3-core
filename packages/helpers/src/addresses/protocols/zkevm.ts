4d9b08887dc40e26f916c156cedb4854db02c418
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-10-23 16:26:53 -0300

    Helpers: Add more Base and zkEVM addresses (#116)

---
+++ packages/helpers/src/addresses/protocols/zkevm.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-secrets/no-secrets */
+
+export const BALANCER_V2_VAULT = '0xBA12222222228d8Ba445958a75a0704d566BF2C8'
+export const PARASWAP_V5_AUGUSTUS = '0xb83b554730d29ce4cb55bb42206c3e2c03e4a40a'
