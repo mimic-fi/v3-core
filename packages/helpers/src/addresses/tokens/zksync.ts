35ebb12b8814399da91d4b65ea563c26a3bf0b29
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-11-03 14:07:37 -0300

    Helpers: Add aurora addresses (#125)

--- packages/helpers/src/addresses/tokens/zksync.ts
+++ packages/helpers/src/addresses/tokens/zksync.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-secrets/no-secrets */
 
-export const WETH = '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91'
+export const WETH = '0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91'
 export const USDC = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'
 export const USDT = '0x493257fD37EDB34451f62EDf8D2a0C418852bA4C'
