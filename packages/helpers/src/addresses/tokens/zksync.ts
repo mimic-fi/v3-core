8eef69d15a1669f87db4066696a013b5f8d27c18
Ramiro Gonzalez <ramiro@mimic.fi>
2024-03-22 11:22:38 -0300

    chore: refactor USDC addresses

--- packages/helpers/src/addresses/tokens/zksync.ts
+++ packages/helpers/src/addresses/tokens/zksync.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-secrets/no-secrets */
 
 export const WETH = '0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91'
-export const USDC = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'
+export const USDCe = '0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'
 export const USDT = '0x493257fD37EDB34451f62EDf8D2a0C418852bA4C'
