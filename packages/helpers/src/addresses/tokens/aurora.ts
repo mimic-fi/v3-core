48f0d259c4e367997ef941abe34209301d0e24d8
Ramiro Gonzalez <ramiro@xivis.com>
2024-03-22 19:34:39 +0100

    Helpers: Update USDC addresses (#144)

--- packages/helpers/src/addresses/tokens/aurora.ts
+++ packages/helpers/src/addresses/tokens/aurora.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-secrets/no-secrets */
 
 export const WETH = '0xC9BdeEd33CD01541e1eeD10f90519d2C06Fe3feB'
-export const USDC = '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802'
+export const USDCe = '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802'
 export const USDT = '0x4988a896b1227218e4A686fdE5EabdcAbd91571f'
 export const DAI = '0xe3520349F477A5F6EB06107066048508498A291b'
--- packages/helpers/src/addresses/tokens/zkevm.ts
+++ packages/helpers/src/addresses/tokens/zkevm.ts
@@ -2,5 +2,5 @@
 
 export const WETH = '0x4F9A0e7FD2Bf6067db6994CF12E4495Df938E6e9'
 export const DAI = '0xc5015b9d9161dca7e18e32f6f25c4ad850731fd4'
-export const USDC = '0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035'
+export const USDCe = '0x37eAA0eF3549a5Bb7D431be78a3D99BD360d19e5'
 export const USDT = '0x1e4a5963abfd975d8c9021ce480b42188849d41d'
